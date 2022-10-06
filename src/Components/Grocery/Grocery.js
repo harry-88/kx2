2ab8364284da8a87ce457384495d87f3e7d0c02d
shehryartalat <shehryartalal41@gmail.com>
2022-10-06 16:45:50 +0500

    create and show warehouse is ready

--- src/Components/Grocery/Grocery.js
+++ src/Components/Grocery/Grocery.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 export default function Grocery(props) {
   return (
-    <div>
-      
-      <div className={props.pageFullWidth === false ? 'fullWidth' : 'smalWidth'}>
-            <h4 className='bg-light shadow-lg  bg-body rounded  p-2 mt-2'>Grocery</h4>
+    // <div>
+
+    //   <div className={props.pageFullWidth === false ? 'fullWidth' : 'smalWidth'}>
+    //         <h4 className='bg-light shadow-lg  bg-body rounded  p-2 mt-2'>Grocery</h4>
+    //     </div>
+    // </div>
+
+    <div className={props.pageFullWidth === false ? 'fullWidth' : 'smalWidth'}>
+      <div class="d-flex bd-highlight bg-light shadow-lg  bg-body rounded mx-4 p-2 mt-2">
+        <div class="p-2 bd-highlight"><h4 className=''>Grocery</h4></div>
+        <div class="ms-auto p-2 bd-highlight">
+          <button className='btn btn-light btn-outline-warning text-dark'>Add Grocery</button>
         </div>
+      </div>
+
+
     </div>
   )
 }
