335e06f8bc8a9b37c4dd2038a20661221a798f87
Muhammad Haris <harisbro88@gmail.com>
2022-10-08 12:13:06 +0500

    add catagory modal is working,w8ing for API.

--- src/Components/Grocery/AddGroceryModal.js
+++ src/Components/Grocery/AddGroceryModal.js
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { addCatagoryRecord } from '../../GetData/Grocerys';
 
 export default function AddGroceryModal(props) {
 
     const onChange = (e) => {
         setModalInputValue({ ...inputModalValue, [e.target.name]: e.target.value });
     }
-    
+
 
     const [inputModalValue, setModalInputValue] = useState({
-        Name: '',
-        PersonName: '',
-        PersonPhone: '',
-        Location: '',
-        Longitude: '',
-        Latitude: '',
+        name: '',
     })
 
+
+    const addCatagory = async(e) => {
+        if (inputModalValue.name === '') {
+            toast.error("Fields is required.")
+        }
+        else {
+            const result  = await addCatagoryRecord('0',inputModalValue);
+            if(result.code === 200)
+            {
+                props.getAllCatagory();
+
+            }
+            else{
+                toast.warning("There is some error in server side.")
+            }
+        }
+    }
 
     return (
         <Modal
@@ -29,58 +44,21 @@ export default function AddGroceryModal(props) {
             </div>
             <div className="col-12">
                 <div className='addWarehouse'>
-                    {/* <h6 className='text-head'>Warehouse Type</h6> */}
 
                     <form>
-                        <div className='row'>
-                            <div className="col-lg-6 col-md-6 col-sm-12">
-                                <div className="row align-items-center mt-3">
-                                    <label for="">Name</label>
-                                    <div className="col-12">
-                                        <input type="text" className="form-control mt-1" id="inputcode" name='Name' value={inputModalValue.Name} onChange={onChange} placeholder="Name" />
-                                    </div>
-
-                                    <label for="" className='mt-1'>Person Name</label>
-                                    <div className="col-12">
-                                        <input type="text" className="form-control mt-1" id="inputcode" name='PersonName' value={inputModalValue.PersonName} onChange={onChange} placeholder="Person Name" />
-                                    </div>
-
-                                    <label for="" className='mt-1'>Person Phone</label>
-                                    <div className="col-12">
-                                        <input type="text" className="form-control mt-1" id="inputcode" name='PersonPhone' value={inputModalValue.PersonPhone} onChange={onChange} placeholder="Person Phone" />
-                                    </div>
-                                </div>
+                        <div className="row align-items-center mt-3">
+                            <label for="">Catagory Name</label>
+                            <div className="col-12">
+                                <input type="text" className="form-control mt-1" id="inputcode" name='name' value={inputModalValue.name} onChange={onChange} placeholder="Catagory Name" />
                             </div>
-
-                            <div className="col-lg-6 col-md-6 col-sm-12">
-                                <div className="row align-items-center mt-3">
-                                    <label for="" >Location</label>
-                                    <div className="col-12">
-                                        <input type="text" className="form-control mt-1" id="inputcode" name='Location' value={inputModalValue.Location} onChange={onChange} placeholder="Location" />
-                                    </div>
-
-                                    <label for="" className='mt-1'>Longitude</label>
-                                    <div className="col-12">
-                                        <input type="text" className="form-control mt-1" id="inputcode" name='Longitude' value={inputModalValue.Longitude} onChange={onChange} placeholder="Longitude" />
-                                    </div>
-
-                                    <label for="" className='mt-1'>Latitude</label>
-                                    <div className="col-12">
-                                        <input type="text" className="form-control mt-1" id="inputcode" name='Latitude' value={inputModalValue.Latitude} onChange={onChange} placeholder="Latitude" />
-                                    </div>
-
-                                </div>
-                            </div>
-
-
                         </div>
-
+                        <div className="modal-footer col-12">
+                            <button type="button" onClick={props.onHide} className="btn cancelbtn" data-dismiss="modal">Cancel</button>
+                            <button type="button" onClick={addCatagory} className='btn savebtn'>Save</button>
+                        </div>
                     </form>
 
-                </div>
-                <div className="modal-footer col-12">
-                    <button type="button" onClick={props.onHide} className="btn cancelbtn" data-dismiss="modal">Cancel</button>
-                    <button type="button" className='btn savebtn'>Save</button>
+                    <ToastContainer className={'mt-5'} />
                 </div>
             </div >
 
